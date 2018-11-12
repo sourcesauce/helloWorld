0e9dd93907ef921a254068b18ab5b6777754e623
sourcesauce <sshlc940503@naver.com>
2018-11-12 12:42:10 +0900

    Git 테스트
    System.out.println 만 추가

--- src/GitTest.java
+++ src/GitTest.java
@@ -3,6 +3,13 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("Git Test");
+		System.out.println("Git Test");
+		System.out.println("Git Test");
+		System.out.println("Git Test");
+		System.out.println("Git Test");
+		System.out.println("Git Test");
+		System.out.println("Git Test");
+		System.out.println("Git Test");
 
 	}
 
